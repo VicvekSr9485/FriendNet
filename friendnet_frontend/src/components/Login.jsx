7b0ebfbcdce4b25564cb0ff65fab29a48345ee36
VicvekSr9485 <olamideoso65@gmail.com>
2023-09-03 18:25:37 +0100

    add more features

--- friendnet_frontend/src/components/Login.jsx
+++ friendnet_frontend/src/components/Login.jsx
@@ -1,74 +1,81 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
-import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png';
-
-//import { client } from '../client';
+import {FcGoogle}  from 'react-icons/fc' 
+import sharedVideo from '../assets/share.mp4'
+import Logo from '../assets/logowhite.png'
+import {client} from '../client';
+import { useNavigate } from 'react-router-dom'
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
-  //const navigate = useNavigate();
 
-  const ResponseGoogle = (response) => {
-    localStorage.setItem('user', JSON.stringify(response.profileObj))
+    const navigate = useNavigate()
 
-    const { name, googleId, imageUrl } = response.profileObj;
+    const responseGoogle = (response) => {
 
-    const doc = {
-      _id: googleId,
-      _type: 'user',
-      userName: name,
-      image: imageUrl,
+    
+    try{
+        localStorage.setItem('user', JSON.stringify(response.profileObj))
+        
+        var decodedHeader = jwt_decode(response.credential);
+        console.log(decodedHeader)
+        //destrcure some of the props from that response
+        const {name, sub, imageUrl } = decodedHeader
+      
+        const doc ={
+            _id: sub,
+            _type: 'user',
+            userName: name,
+            image: imageUrl, 
+        } 
+        
+        client.createIfNotExists(doc)
+        .then(() =>{
+            navigate('/', {replace : true} )
+        
+        })
+        .catch(error => console.log(error))
     }
-
-    // client.createIfNotExists(doc)
-    // .then(() => {
-    //   navigate('/', { replace: true })
-    // })
-  }
- 
-  return (
-    <div  className="flex justify-start items-center flex-col h-screen">
-      <div className="relative w-full h-full">
+    catch (e) {
+        localStorage.clear() //what you need to do incase the jwt is not valid
+        console.log(e) //for your own debugging
+    }
+}
+return (
+<div className='flex justify-start items-center flex-col h-screen'>
+    <div className='relative w-full h-full'>
         <video 
-          src={shareVideo}
-          type="video/mp4"
-          loop
-          controls={false}
-          muted
-          autoPlay
-          className="w-full h-full object-cover"
+            src={sharedVideo}
+            type ='video/mp4'
+            muted
+            loop
+            autoPlay
+            className='w-full h-full object-cover'
         />
-
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-          <div className="p-5">
-            <img src={logo} width="130px" alt="logo" />
-          </div>
-
-          <div className="shadow-2x1">
-            <GoogleLogin
-              clientId=''
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sing in with Google
-                </button>
-              )}
-
-              onSuccess={ResponseGoogle}
-              onFailure={ResponseGoogle}
-              cookiePolicy="single_host_origin"
-            />
-          </div>
+        <div className='absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 bg-blackOverlay'>
+        <div  className='p-5'>
+            <img src ={Logo} alt = 'logo' width='130px'/>
         </div>
-      </div>
+        <div className='shadow-2xl '>
+              <GoogleLogin
+                
+                render={(renderProps) =>(
+                    <button 
+                    onClick={renderProps.onClick} 
+                    disabled = {renderProps.disabled}
+                    className=' bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'>
+                        <FcGoogle className='mr-4 '/>Sign In with Google
+                    </button>
+                )}
+                onSuccess = {responseGoogle}
+                onFailure = {responseGoogle}
+                cookiePolicy = 'single_host_origin'
+                />
+        </div>
+        </div>
+    </div>
     </div>
   )
 }
 
-export default Login;
+export default Login
--- friendnet_frontend/src/index.js
+++ friendnet_frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+// Use createRoot to render your app in React 18
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-        <Router>
-            <App />
-        </Router>,
-    </GoogleOAuthProvider>,
-document.getElementById('root')
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
