c430e85c8b6a220eb1dee17d3a33d9275f383288
VicvekSr9485 <olamideoso65@gmail.com>
2023-09-13 02:50:59 +0100

    add more refactoring

--- friendnet_frontend/src/components/PinDetail.jsx
+++ friendnet_frontend/src/components/PinDetail.jsx
@@ -89,7 +89,7 @@ const PinDetail = ({ user }) => {
               <h1 className="text-4xl font-bold break-words mt-3">
                 {pinDetail.title}
               </h1>
-              <p className="mt-3">{pinDetail.about}</p>
+              <p className="mt-3">{pinDetail?.about}</p>
             </div>
             <Link to={`/user-profile/${pinDetail?.postedBy._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg ">
               <img src={pinDetail?.postedBy.image} className="w-10 h-10 rounded-full" alt="user-profile" />
